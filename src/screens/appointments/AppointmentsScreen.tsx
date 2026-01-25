8750f883161844fc2d3533578956646a09e61577
susicharis7 <susicharis99@gmail.com>
2026-01-25 22:33:12 +0100

    Added Appointments Screen - Default Export Currently, Added for import to work

---
+++ src/screens/appointments/AppointmentsScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export function AppointmentsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 20 }}>Appointments Screen</Text>
+    </View>
+  );
+}
