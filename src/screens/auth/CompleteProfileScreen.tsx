12d58a36c43d8275e335762680180a1a89de944e
susicharis7 <susicharis99@gmail.com>
2026-01-25 22:33:53 +0100

    Added auth Folder - Login Logic Redirected to services/

---
+++ src/screens/auth/CompleteProfileScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+export default function CompleteProfileScreen() {
+  return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+    >
+      <Text style={{ fontSize: 22 }}>
+        Complete Profile 👤
+      </Text>
+    </View>
+  );
+}
---
+++ src/screens/auth/LoginScreen.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { login } from '../../services/auth-service'
+import { loginStyles as styles } from '../../styles/screens/login-styles';
+
+export default function LoginScreen({ navigation }: any) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      Alert.alert("Error", "Please enter email and password.");
+      return;
+    }
+
+
+    try {
+      await login(email, password);
+    } catch (error: any) {
+      Alert.alert('Login failed', error.message);
+    }
+  };
+
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Login
+      </Text>
+
+      <TextInput
+        placeholder="Please enter your Email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+
+      <TextInput 
+        placeholder="Please enter your Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+      />
+
+      <View style={styles.buttonSpacing}>
+        <Button title="Login" onPress={handleLogin}/>
+      </View>
+      
+    </View>
+  )
+}
