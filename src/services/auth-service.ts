2b2a84740e5b07826a723ece6b7a2137ce21dec4
susicharis7 <susicharis99@gmail.com>
2026-01-25 22:35:00 +0100

    Login Logic is here now, also added Logout Logic, and Get Current User

---
+++ src/services/auth-service.ts
@@ -0,0 +1,32 @@
+import { 
+    signInWithEmailAndPassword,
+    signOut,
+    createUserWithEmailAndPassword,
+    User,
+} from 'firebase/auth';
+
+import { firebaseAuth } from './firebase';
+
+
+export const login = async(
+    email: string,
+    password: string
+): Promise<User> => {
+    const userCredential = await signInWithEmailAndPassword(
+        firebaseAuth,
+        email,
+        password
+    );
+
+    return userCredential.user;
+};
+
+/* Logout Current User */
+export const logout = async(): Promise<void> => {
+    await signOut(firebaseAuth);
+};
+
+/* Get Current User */
+export const getCurrentUser = (): User | null => {
+    return firebaseAuth.currentUser;
+}
