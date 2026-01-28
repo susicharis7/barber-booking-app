f3d49b279df5479389f4b5c384f1e395b72f4a1e
susicharis7 <susicharis99@gmail.com>
2026-01-28 11:06:01 +0100

    Updated Auth Context - Fetch is working, and we are getting result from backend

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -22,10 +22,38 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
             setUser(firebaseUser);
             setLoading(false);
 
-            if (firebaseUser) {
-                const token = await firebaseUser.getIdToken();
-                console.log("ID Token: ", token);
+            if (!firebaseUser) {
+                return;
             }
+
+            try {
+                const token = await firebaseUser.getIdToken();
+                console.log("Token: ", token);
+                const res = await fetch('http://192.168.1.79:5000/auth/me', {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                const contentType = res.headers.get('content-type') || '';
+                const raw = await res.text();
+
+                if (!res.ok) {
+                    console.log('Backend Auth Error:', res.status, raw);
+                    return;
+                }
+
+                const data = contentType.includes('application/json')
+                    ? JSON.parse(raw)
+                    : { raw };
+
+                console.log("Backend User: ", data);
+            } catch (error) {
+                console.log("Backend Auth Error: " ,error);
+            }
+
+            
         });
 
         return unsubscribe;
