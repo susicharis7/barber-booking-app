afef12fdce4d2ac11235c9ce1646a627d975f9cc
susicharis7 <susicharis99@gmail.com>
2026-01-29 19:43:09 +0100

    Updated index.ts to run the server only
    In this new folder structure which works perfect for this project, it only runs the server and loads .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,60 +1,14 @@
-import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
-
 dotenv.config();
-import './db/pool'; 
 
-import { firebaseAdminAuth } from './firebase/firebase-admin';
+import './db/pool';
+import app from './app';
 
-
-const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-
-// Health Check Route
-app.get('/health', (req, res) => {
-  res.json({ status: 'OK', timestamp: new Date().toISOString() });
-});
-
-
-
-
-app.get("/auth/me", async (req, res) => {
-  try {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      console.log("AUTH /auth/me: missing Authorization header");
-      return res.status(401).json({ message: "No token provided" });
-    }
-
-    const token = authHeader.split(" ")[1];
-    console.log("AUTH /auth/me Token:", token);
-
-    const decodedToken = await firebaseAdminAuth.verifyIdToken(token);
-    console.log("AUTH /auth/me Backend User:", {
-      uid: decodedToken.uid,
-      email: decodedToken.email,
-      provider: decodedToken.firebase.sign_in_provider,
-    });
-
-    return res.json({
-      uid: decodedToken.uid,
-      email: decodedToken.email,
-      provider: decodedToken.firebase.sign_in_provider,
-    });
-  } catch (error) {
-    console.error("VERIFY TOKEN ERROR:", error);
-    return res.status(401).json({ message: "Invalid token" });
-  }
-});
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+/* Just runs the server, loads .env, runs server on PORT */
