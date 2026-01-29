45eedfbaf1bb58fc23e6849eed9931e6d3e63c22
susicharis7 <susicharis99@gmail.com>
2026-01-29 19:47:58 +0100

    Service for the User
    Creates new user in DB, finds user by Firebase UID & Email

---
+++ backend/src/modules/user/userService.ts
@@ -0,0 +1,51 @@
+import { pool } from '../../db/pool';
+
+export interface CreateUserData {
+    firebase_uid: string; 
+    email: string;
+    first_name: string;
+    last_name: string; 
+}
+
+export interface User {
+    id: number;
+    firebase_uid: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    created_at: Date;
+}
+
+/* Creates new User in Postgre */
+export const createUser = async (userData: CreateUserData): Promise<User> => {
+    const { firebase_uid, email, first_name, last_name } = userData;
+
+    const result = await pool.query(
+        `INSERT INTO users (firebase_uid, email, first_name, last_name)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *`,
+        [firebase_uid, email, first_name, last_name]
+    );
+
+    return result.rows[0];
+};
+
+/* Finds user by Firebase UID */
+export const findUserByFirebaseUID = async (firebase_uid: string): Promise<User | null> => {
+    const result = await pool.query(
+        `SELECT * FROM users WHERE firebase_uid = $1`,
+        [firebase_uid]
+    );
+
+    return result.rows[0] || null;
+}
+
+/* Finds user by EMAIL */
+export const findUserByEmail = async (email: string): Promise<User | null> => {
+    const result = await pool.query(
+        `SELECT * FROM users WHERE email = $1`,
+        [email]
+    );
+
+    return result.rows[0] || null;
+}
