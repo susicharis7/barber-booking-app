3807783d4b06ca0c46f33cbf510d518885f6a9ab
susicharis7 <susicharis99@gmail.com>
2026-01-30 12:59:07 +0100

    Updates

---
+++ mobile/src/screens/services/ServiceScreen.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+
+export default function ServiceScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffffff',
+      }}
+    >
+      <Text style={{ fontSize: 22, color: '#0f172a' }}>
+        Services Screen
+      </Text>
+    </View>
+  );
+}
