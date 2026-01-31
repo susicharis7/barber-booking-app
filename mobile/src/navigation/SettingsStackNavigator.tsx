bc7f7b9bd6fae6b6a32b470311b2b07976e2cdb4
susicharis7 <susicharis99@gmail.com>
2026-01-31 12:04:02 +0100

    Added more screens to the Stack Navigator

--- mobile/src/navigation/SettingsStackNavigator.tsx
+++ mobile/src/navigation/SettingsStackNavigator.tsx
@@ -3,6 +3,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SettingsScreen from '../screens/settings/SettingsScreen';
 import UserProfileScreen from '../screens/settings/UserProfileScreen';
 import MyAccountScreen from '../screens/settings/MyAccountScreen';
+import AboutAppScreen from '../screens/settings/AboutAppScreen';
+import TermsOfUseScreen from '../screens/settings/TermsOfUseScreen';
+import PrivacyPolicyScreen from '../screens/settings/PrivacyPolicyScreen';
+
+
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +17,11 @@ export default function SettingsStackNavigator() {
       <Stack.Screen name="SettingsMain" component={SettingsScreen} />
       <Stack.Screen name="UserProfile" component={UserProfileScreen} />
       <Stack.Screen name="MyAccount" component={MyAccountScreen} />
+      <Stack.Screen name="AboutApp" component={AboutAppScreen} />
+      <Stack.Screen name="TermsOfUse" component={TermsOfUseScreen} />
+      <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicyScreen} />
+
+
     </Stack.Navigator>
   );
 }
