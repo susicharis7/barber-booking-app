66b6af88487c0a0d1778d13893f544a6c9ed0d10
susicharis7 <susicharis99@gmail.com>
2026-02-03 21:56:15 +0100

    Interval for checking every 2 mins if the appointment should be completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,4 +11,19 @@ app.listen(PORT, () => {
 });
 
 
-/* Just runs the server, loads .env, runs server on PORT */
+// Check every 2 minutes if the appointment should be completed 
+import { markPastAppointmentsCompleted } from './modules/appointments/appointmentsService';
+const runEveryMS = 120000;
+
+setInterval(async () => {
+  try {
+    const updated = await markPastAppointmentsCompleted();
+
+    if (updated > 0) {
+      console.log(`Marked ${updated} appointments as completed`)
+    };
+  } catch (err) {
+        console.error('Auto-complete appointments failed:', err);
+
+  }
+}, runEveryMS)
