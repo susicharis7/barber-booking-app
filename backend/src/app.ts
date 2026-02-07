0739b4b4033079d416b423e9ad14bf61845d11d7
susicharis7 <susicharis99@gmail.com>
2026-02-07 13:36:22 +0100

    Added browser-layer control (just in case)
    We still allow mobile, but I wanted to restrict random websites from using my API in the browser apps, and I only allowed origins that I explicitly listed in .env file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,25 @@ import waitingListRoutes from './modules/waitingList/waitingListRoutes';
 const app = express();
 
 /* Middleware Configuration */
-app.use(cors());
+const allowedOrigins = (process.env.FRONTEND_URLS || 'https://localhost:3000')
+    .split(',')
+    .map((o) => o.trim())
+    .filter(Boolean);
+
+app.use(
+    cors({ 
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true); // mobile, postman
+            if (allowedOrigins.includes(origin)) return callback(null,true);
+            return callback(new Error('Not allowed by CORS'));
+        },
+
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: false,  // true only if we use cookie auth
+    })
+)
+
 app.use(express.json());
 app.use(express.urlencoded( { extended: false }));
 
