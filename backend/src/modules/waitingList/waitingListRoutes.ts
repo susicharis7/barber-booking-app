63f3a1fe949e631ce1dddf3aa1c00d5468f4c645
susicharis7 <susicharis99@gmail.com>
2026-02-12 16:18:04 +0100

    Minor update

--- backend/src/modules/waitingList/waitingListRoutes.ts
+++ backend/src/modules/waitingList/waitingListRoutes.ts
@@ -1,30 +1,40 @@
 import { Router } from 'express';
 import { verifyToken } from '../../middleware/authMiddleware';
+import { requireRegisteredUser } from '../../middleware/rbacMiddleware';
 import * as waitingListController from './waitingListController';
 import { validateRequest } from '../../middleware/validateRequest';
-import { cancelWaitingListValidation, createWaitingListValidation } from './waitingListValidation';
+import {
+  cancelWaitingListValidation,
+  createWaitingListValidation,
+} from './waitingListValidation';
 import { createWaitingListLimiter } from '../../middleware/rateLimiters';
-
 
 const router = Router();
 
-router.get('/', verifyToken, waitingListController.getWaitingList);
+router.get(
+  '/',
+  verifyToken,
+  requireRegisteredUser,
+  waitingListController.getWaitingList
+);
 
 router.post(
-    '/', 
-    verifyToken, 
-    createWaitingListLimiter,
-    createWaitingListValidation,
-    validateRequest,
-    waitingListController.createWaitingList
+  '/',
+  verifyToken,
+  requireRegisteredUser,
+  createWaitingListLimiter,
+  createWaitingListValidation,
+  validateRequest,
+  waitingListController.createWaitingList
 );
 
 router.put(
-    '/:id/cancel', 
-    verifyToken, 
-    cancelWaitingListValidation,
-    validateRequest,
-    waitingListController.cancelWaitingList
+  '/:id/cancel',
+  verifyToken,
+  requireRegisteredUser,
+  cancelWaitingListValidation,
+  validateRequest,
+  waitingListController.cancelWaitingList
 );
 
 export default router;
