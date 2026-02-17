6594d3059944ddf73d4670f08d9ed23a1176ffc5
susicharis7 <susicharis99@gmail.com>
2026-02-17 23:50:27 +0100

    Reorganize AUTH Services into dedicated module

---
+++ mobile/src/services/auth/auth-service.ts
@@ -0,0 +1,43 @@
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+  deleteUser,
+  User,
+} from 'firebase/auth';
+
+import { firebaseAuth } from './firebase';
+
+export const login = async (email: string, password: string): Promise<User> => {
+  const userCredential = await signInWithEmailAndPassword(firebaseAuth, email, password);
+
+  return userCredential.user;
+};
+
+/* Logout Current User */
+export const logout = async (): Promise<void> => {
+  await signOut(firebaseAuth);
+};
+
+/* Get Current User */
+export const getCurrentUser = (): User | null => {
+  return firebaseAuth.currentUser;
+};
+
+/* Registration */
+export const registerWithEmailAndPassword = async (
+  email: string,
+  password: string,
+): Promise<User> => {
+  const userCredential = await createUserWithEmailAndPassword(firebaseAuth, email.trim(), password);
+
+  return userCredential.user;
+};
+
+/* Deletes currently signed in Firebase User => for Registration rollback */
+export const deleteCurrentUserAccount = async (): Promise<void> => {
+  const currentUser = firebaseAuth.currentUser;
+
+  if (!currentUser) return;
+  await deleteUser(currentUser);
+};
---
+++ mobile/src/services/auth/firebase.ts
@@ -0,0 +1,34 @@
+import { initializeApp } from 'firebase/app';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { browserLocalPersistence, getAuth, initializeAuth, type Auth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDx8yai5AAU7Wl0nkIScn3oQphP6a5K940',
+  authDomain: 'barber-booking-app-199a4.firebaseapp.com',
+  projectId: 'barber-booking-app-199a4',
+  storageBucket: 'barber-booking-app-199a4.firebasestorage.app',
+  messagingSenderId: '1070876676064',
+  appId: '1:1070876676064:web:d074525d9d49744249e585',
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+let firebaseAuth: Auth;
+
+try {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { getReactNativePersistence } = require('firebase/auth');
+  firebaseAuth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+} catch {
+  firebaseAuth = getAuth(app);
+  firebaseAuth.setPersistence(browserLocalPersistence);
+}
+
+export { app, firebaseAuth, storage };
+export const firebaseDB = getFirestore(app);
